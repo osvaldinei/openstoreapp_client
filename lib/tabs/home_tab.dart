4d9943a77e490909614d61559fc5d64f4eb9f600
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2020-12-08 14:04:45 -0300

    Firestore Integration

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
@@ -24,6 +26,33 @@ class HomeTab extends StatelessWidget {
               title: const Text("Novidades"),
               centerTitle: true,
             ),
+          ),
+          
+          FutureBuilder<QuerySnapshot>(
+            future: FirebaseFirestore.instance
+                .collection("home")
+                .orderBy("pos")
+                .get(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData)
+                return SliverToBoxAdapter(
+                    child: Container(
+                  alignment: Alignment.center,
+                  height: 200.0,
+                  child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
+                ));
+              else {
+                print(snapshot.data.docs.length);
+                return SliverToBoxAdapter(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 200,
+                    child: Container(),
+                    )
+                );
+              }
+            },
           )
         ])
       ],
