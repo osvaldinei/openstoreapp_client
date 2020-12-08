58afe9605ee233b47cc2c4f631887981d02f2ce0
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2020-12-08 16:13:12 -0300

    Structuring Product Data

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,25 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:openstoreapp_client/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
       future: FirebaseFirestore.instance.collection("products").get(),
-      builder: (context, snapshot){
-        if(!snapshot.hasData){
-          return Center(child: CircularProgressIndicator(),);
-        }else{
-          return ListView(
-            children:[
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
 
-            ]
+          var dividedTiles = ListTile.divideTiles(
+                  tiles: snapshot.data.docs.map((doc) {
+                    return CategoryTile(doc);
+                  }).toList(),
+                  color: Colors.grey[500])
+              .toList();
+
+          return ListView(
+            children: dividedTiles,
           );
         }
-
       },
-      
-      
-      );
+    );
   }
 }
