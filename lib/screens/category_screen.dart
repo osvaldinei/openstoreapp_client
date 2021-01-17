f6a30ce4f551baf4eae612e902e0fa3de4312926
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-01-17 20:15:21 -0300

    Grid and Product List

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:openstoreapp_client/datas/product_data.dart';
+import 'package:openstoreapp_client/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -13,27 +15,64 @@ class CategoryScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-            title: Text(snapshot.data()["title"]),
-            centerTitle: true,
-            bottom: TabBar(
-              indicatorColor: Colors.white,
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.grid_on),
-                ),
-                Tab(
-                  icon: Icon(Icons.list),
-                )
-              ],
-            )),
-            body: TabBarView(
-              physics: NeverScrollableScrollPhysics(),
-              children: [
-              Container(color: Colors.green),
-              Container(color: Colors.red)
-            ],),
-      ),
+          appBar: AppBar(
+              title: Text(snapshot.data()["title"]),
+              centerTitle: true,
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.grid_on),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.list),
+                  )
+                ],
+              )),
+          body: FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance
+                  .collection("products")
+                  .doc(snapshot.id)
+                  .collection("items")
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                else
+                  return TabBarView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      GridView.builder(
+                        padding: EdgeInsets.all(4.0),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 4.0,
+                          crossAxisSpacing: 4.0,
+                          childAspectRatio: 0.65,
+                        ),
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (context, index) {
+                          return ProductTile(
+                              "grid",
+                              ProductData.fromDocument(
+                                  snapshot.data.docs[index]));
+                        },
+                      ),
+                      ListView.builder(
+                        padding: EdgeInsets.all(4.0),
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (context, index) {
+                          return ProductTile(
+                              "list",
+                              ProductData.fromDocument(
+                                  snapshot.data.docs[index]));
+                        },
+                      )
+                    ],
+                  );
+              })),
     );
   }
 }
