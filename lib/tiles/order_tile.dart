d3862133108d3d4962ff129b89a29b1819bce688
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2021-02-25 21:50:20 -0300

    Created Card of Order

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,15 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
 
-  final String OrderId;
+  final String orderId;
 
-  OrderTile(this.OrderId);
+  OrderTile(this.orderId);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(OrderId)
+      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: StreamBuilder<DocumentSnapshot>(
+          
+          stream: Firestore.instance.collection("orders").document(orderId).snapshots(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData)
+              return Center(child: CircularProgressIndicator(),);
+            else{
+              return Column(children: [
+                Text("Código do pedido: ${snapshot.data.documentID}", style: TextStyle(fontWeight: FontWeight.bold),)
+              ],);
+            }  
+          }, 
+        )
+      )
     );
   }
 }
